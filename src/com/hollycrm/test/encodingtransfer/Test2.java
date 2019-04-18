a101966f24e68c37ec43573b702bd430e77e6772
cs_chengpf <chengpf@hollycrm.com>
2019-04-18 09:52:33 +0800

    Thursday 4-18

--- src/com/hollycrm/test/encodingtransfer/Test2.java
+++ src/com/hollycrm/test/encodingtransfer/Test2.java
@@ -7,13 +7,10 @@ public class Test2 {
     public static void main(String[] args) throws Exception {
         InputStreamReader in = new InputStreamReader(new FileInputStream("d:/abc/f4"));
         int s ;
-        long start = System.currentTimeMillis();
-        while((s=in.read())!=-1){
-            System.out.print((char) s);
+        char[] buff = new char[2];
+        while((s=in.read(buff))!=-1){
+            System.out.print(new String(buff,0,s));
         }
-        long end = System.currentTimeMillis();
-        System.out.println("\n----------------------------------------------------------------");
-        System.out.println(end-start);
         in.close();
     }
 }
