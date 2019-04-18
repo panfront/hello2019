03c80c37fdf16acf9b855acbd61f25db6c9bab36
cs_chengpf <chengpf@hollycrm.com>
2019-04-18 09:57:52 +0800

    Thursday 4-18 4

--- src/com/hollycrm/test/encodingtransfer/FundamentalDemo.java
+++ src/com/hollycrm/test/encodingtransfer/FundamentalDemo.java
@@ -4,5 +4,6 @@ public class FundamentalDemo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("2nd");
+        System.out.println("3rd");
     }
 }
