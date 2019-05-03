26adecce4d76c940efa275287c960f5082ae201e
Pan <panfront@msn.cn>
2019-05-03 21:14:16 +0800

    更新 NewDemo.java

--- src/com/hollycrm/test/NewDemo.java
+++ src/com/hollycrm/test/NewDemo.java
@@ -4,5 +4,6 @@ public class NewDemo {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		System.out.println("hello world3");
+		System.out.println("gitee 1");
 	}
 }
