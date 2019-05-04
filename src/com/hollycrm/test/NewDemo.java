89f1673f0cff30adddf5c93c0e23e3ebbdc1c858
pan <panfront@msn.cn>
2019-05-04 22:52:57 +0800

    second commit3

--- src/com/hollycrm/test/NewDemo.java
+++ src/com/hollycrm/test/NewDemo.java
@@ -3,7 +3,7 @@ package com.hollycrm.test;
 public class NewDemo {
 	public static void main(String[] args) {
 		System.out.println("workspace1");
-		System.out.println("wo2");
+		System.out.println("workspace2");
 		System.out.println("gitee1");
 		System.out.println("workspace3");
 	}
