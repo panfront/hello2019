b418d049dd01249b8b7cd8dd713486dcca1d89d5
cs_chengpf <chengpf@hollycrm.com>
2019-05-06 16:46:01 +0800

    workspace8

--- src/com/hollycrm/test/NewDemo.java
+++ src/com/hollycrm/test/NewDemo.java
@@ -1,17 +1,31 @@
 package com.hollycrm.test;
 
+import java.util.Scanner;
+
 public class NewDemo {
 	public static void main(String[] args) {
-		System.out.println("workspace1");
-		System.out.println("workspace2");
-		System.out.println("gitee1");
-		System.out.println("workspace3");
-		System.out.println("workspace4");
-		System.out.println("workspace5");
-		System.out.println("gitee2");
-		System.out.println("workspace6");
-		System.out.println("gitee3");
-		System.out.println("gitee4");
-		System.out.println("workspace7");
+		int abccount=0;//字母数
+		int spacecount=0;//空格数
+		int numcount=0;//数字数
+		int othercount=0;//其他字符数
+
+		String toString=new Scanner(System.in).nextLine();
+		char [] ch=toString.toCharArray();
+
+		for(int i=0;i<ch.length;i++) {
+			if(Character.isLetter(ch[i])) {
+				abccount++;
+			}else if(Character.isDigit(ch[i])) {
+				numcount++;
+			}else if(Character.isSpaceChar(ch[i])){
+				spacecount++;
+			}else {
+				othercount++;
+			}
+		}
+		System.out.println("中英文字符数量："+abccount);
+		System.out.println("空格数："+spacecount);
+		System.out.println("数字数量："+numcount);
+		System.out.println("其他字符数量:"+othercount);
 	}
 }
