3dfad301e2243c7acf77d3091d10595dec30ecfb
cs_chengpf <chengpf@hollycrm.com>
2019-06-17 16:48:00 +0800

    idea 3rd提交 2019-6-17 16：47

--- src/com/hollycrm/test/ConflictTest.java
+++ src/com/hollycrm/test/ConflictTest.java
@@ -6,6 +6,6 @@ public class ConflictTest {
         System.out.println("git 提交 2019-6-17 16：28");
         System.out.println("idea 2nd提交 2019-6-17 16：29");
         System.out.println("git 2nd提交 2019-6-17 16：38");
-        
+        System.out.println("idea 3rd提交 2019-6-17 16：29");
     }
 }
