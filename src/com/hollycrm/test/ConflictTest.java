7567f6378c763ba014bf8ca436a0a72db783e5ee
panfront <46662236+panfront@users.noreply.github.com>
2019-06-17 16:28:59 +0800

    Update ConflictTest.java

--- src/com/hollycrm/test/ConflictTest.java
+++ src/com/hollycrm/test/ConflictTest.java
@@ -3,5 +3,6 @@ package com.hollycrm.test;
 public class ConflictTest {
     public static void main(String[] args) {
         System.out.println("idea 提交 2019-6-17 16：23");
+        System.out.println("git 提交 2019-6-17 16：28");
     }
 }
